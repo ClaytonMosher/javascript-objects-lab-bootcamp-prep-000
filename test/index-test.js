8d78e95069eb2c2b0e146cd74faf94ee1b5b412b
clayton j mosher <mosherclayton@gmail.com>
2019-02-10 05:01:29 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,28 @@
 /*global describe, it */
 
+var recipes = new Object();
+
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+}
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
+}
+
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+
+ }
+
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
+
 describe('Objects', function() {
   it('defines a `recipes` object', function() {
     expect(typeof recipes).toEqual('object')
